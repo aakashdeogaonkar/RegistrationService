8188dae37fb145fe125e1432386489591b0fbb3f
Aakash Deogaonkar <deogaonkar.a@husky.neu.edu>
2020-02-18 00:36:18 -0500

    GetUsers successful

--- user-registration/src/app/search-delete/search-delete.component.ts
+++ user-registration/src/app/search-delete/search-delete.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserRegistrationService } from '../user-registration.service';
 
 @Component({
   selector: 'app-search-delete',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-delete.component.css']
 })
 export class SearchDeleteComponent implements OnInit {
+  users: any;
 
-  constructor() { }
+  constructor(private service: UserRegistrationService) { }
 
   ngOnInit() {
+    let resp = this.service.getUsers();
+    resp.subscribe((data)=> this.users = data);
   }
 
 }
--- user-registration/src/app/user-registration.service.ts
+++ user-registration/src/app/user-registration.service.ts
@@ -11,4 +11,16 @@ export class UserRegistrationService {
   public doRegistration(user) {
     return this.http.post("http://localhost:8080/register", user, {responseType: 'text' as 'json'});
   }
+
+  public getUsers() {
+    return this.http.get("http://localhost:8080/getAllUsers")
+  }
+
+  public getUserByEmail(email) {
+    return this.http.get("http://localhost:8080/findUser/"+email);
+  }
+
+  public deleteUser(id) {
+    return this.http.delete("http://localhost:8080/cancel/"+id)
+  }
 }
